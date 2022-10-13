7d6be191a404c52db90a9bbe468417f86539605b
jiglesias2000 <jiglesias.2000@gmail.com>
2022-10-13 13:25:30 -0300

    desactivo react.stricmode

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-
-// import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import '../node_modules/font-awesome/css/font-awesome.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.css'
 
 import "./index.css";
 
