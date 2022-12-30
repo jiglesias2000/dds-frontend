dc3ecf86b650377aefdb0c12ce274d50a3533919
jiglesias2000 <jiglesias.2000@gmail.com>
2022-12-30 20:38:44 -0300

    etapa1 completa

--- src/App.js
+++ src/App.js
@@ -1,93 +1,9 @@
-import "./App.css";
-
-import React from "react";
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
-import { ErrorBoundary } from "react-error-boundary";
-
-
-
-import Menu from "./components/Menu";
-import { Footer } from "./components/Footer";
 import { Inicio } from "./components/Inicio";
-import { ArticulosFamilias } from "./components/articulosfamilias/ArticulosFamilias";
-import { Articulos } from "./components/articulos/Articulos";
-import Login from "./components/login/Login";
-import ErrorB from "./components/ErrorB";
-import { Contador } from "./components/Contador";
-import { ArticulosJWT } from "./components/articulosJWT/ArticulosJWT";
-import RequireAuth from "./components/RequireAuth";
-
-import ModalDialog from "./components/ModalDialog";
-
-window.onerror = (msg, url, line, col, error) => {
-  // Note that col & error are new to the HTML 5 spec and may not be
-  // supported in every browser.  
-  var extra = !col ? "" : "\ncolumn: " + col;
-  extra += !error ? "" : "\nerror: " + error;
-
-  // You can view the information in an alert to see things working like this:
-  let mensaje = "Error: " + msg + "\nurl: " + url + "\nline: " + line + extra;
-  console.error(mensaje);
-  logError(mensaje);
-
-  var suppressErrorAlert = true;
-  // If you return true, then error alerts (like in older versions of
-  // Internet Explorer) will be suppressed.
-  return suppressErrorAlert;
-};
-
-window.onunhandledrejection = (e) => {
-  logError(e);
-};
-
-const logError = (error) => {
-  console.log( error);
-  // eviar al servidor este error para que lo loguee
-};
-
 function App() {
-  
-
-  return (
+return (
     <>
-      <ErrorBoundary FallbackComponent={ErrorB} onError={logError}>
-        <BrowserRouter>
-
-          {/* <Contador /> */}
-          <ModalDialog/>
-
-          
-          <Menu />
-          <div className="divBody">
-            <Routes>
-              <Route path="/inicio" element={<Inicio />} />
-              <Route
-                path="/articulosfamilias"
-                element={<ArticulosFamilias />}
-              />
-              <Route path="/articulos" element={<Articulos />} />
-              <Route
-                path="/articulosjwt"
-                element={
-                  <RequireAuth>
-                    <ArticulosJWT />
-                  </RequireAuth>
-                }
-              />
-              <Route
-                path="/login"
-                element={
-                  <Login />
-                }
-              />
-              <Route path="*" element={<Navigate to="/inicio" replace />} />
-            </Routes>
-          </div>
-          <Footer />
-        </BrowserRouter>
-      </ErrorBoundary>
+        <Inicio/>
     </>
-  );
+);
 }
-
 export default App;
--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -1,24 +1,21 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
 function Inicio() {
   return (
-      <div className="mt-4 p-5 rounded" style={{backgroundColor:"lightgray"}} >
-        <h1>Pymes 2023</h1>
-        <p>Este ejemplo está desarrollado con las siguientes tecnologías:</p>
-        <p>
-          Backend: NodeJs, Express , WebApiRest, Swagger, Sequelize, Sqlite y 
-          multiples capas en Javascript.
-        </p>
-        <p>
-          Frontend: Single Page Aplication, HTML, CSS, Bootstrap, NodeJs,
-          Javascript y React.
-        </p>
-        <Link to="/articulosfamilias" className="btn btn-lg btn-primary">
-          <i className="fa fa-search"> </i>  Ver Articulos Familias
-        </Link>
-      </div>
+    <div className="mt-4 p-5 rounded" style={{ backgroundColor: "lightgray" }}>
+      <h1>Pymes 2023</h1>
+      <p>Este ejemplo está desarrollado con las siguientes tecnologías:</p>
+      <p>
+        Backend: NodeJs, Express , WebApiRest, Swagger, Sequelize, Sqlite
+        multiples capas en Javascript/Typescript.
+      </p>
+      <p>
+        Frontend: Single Page Aplication, HTML, CSS, Bootstrap, NodeJs,
+        Javascript y React.
+      </p>
+      <button className="btn btn-lg btn-primary">
+        <i className="fa fa-search"> </i>
+        Ver Articulos Familias
+      </button>
+    </div>
   );
 }
-
 export { Inicio };
