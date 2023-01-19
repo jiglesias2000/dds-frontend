a2bbbc81a2fe812b7c848929c167c1a959337f4f
jiglesias2000 <jiglesias.2000@gmail.com>
2023-01-19 18:32:45 -0300

    etapa final, config https backend.

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 //const urlServidor = "http://localhost:3000"
 //const urlServidor = "https://dds-backend.azurewebsites.net"
-//const urlServidor = "https://webapi.pymes.net.ar"
+const urlServidor = "https://webapi.pymes.net.ar"
 const urlResourceArticulos = urlServidor + "/api/articulos";
 const urlResourceArticulosFamilias = urlServidor + "/api/articulosfamilias";
 const urlResourceArticulosJWT = urlServidor + "/api/jwt/articulos";
